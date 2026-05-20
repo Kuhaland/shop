4de1a053d361eb316be1a95d82b14ae6265fc32b
leehx78 <leehx78@smartscore.kr>
2026-05-20 18:41:48 +0900

    fixed login

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,31 +1,36 @@
 <template>
   <div class="login-page">
+    <!-- Content -->
+    <div class="login-bg"></div>
+    <!-- Content -->
     <div class="login-card">
-      <!-- Content -->
+      <!-- Title -->
       <div class="logo">ADMIN</div>
-      <!-- Content -->
       <div class="title">관리자 로그인</div>
       <!-- Content -->
       <div class="form-group">
-        <label>아이디</label>
-        <input v-model="form.id"
-               type="text"
-               placeholder="아이디를 입력해주세요"
-        />
+        <!-- Content -->
+        <div class="item-content">
+          <label>아이디</label>
+          <input v-model="form.id"
+                 type="text"
+                 placeholder="아이디를 입력해주세요"
+          />
+        </div>
+        <!-- Content -->
+        <div class="item-content">
+          <label>비밀번호</label>
+          <input v-model="form.password"
+                 type="password"
+                 placeholder="비밀번호를 입력해주세요"
+                 @keyup.enter="onLogin"
+          />
+        </div>
       </div>
-      <!-- Content -->
-      <div class="form-group">
-        <label>비밀번호</label>
-        <input v-model="form.password"
-               type="password"
-               placeholder="비밀번호를 입력해주세요"
-               @keyup.enter="onLogin"
-        />
-      </div>
-      <!-- Content -->
+      <!-- Button -->
       <button class="login-btn" @click="onLogin">로그인</button>
-      <!-- Content -->
       <p v-if="errorMessage" class="error-text">{{ errorMessage }}</p>
+      <div class="copyright">Power by leehx78</div>
     </div>
   </div>
 </template>
